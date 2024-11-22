b900db727fa32f53642e4d7bf2de68dd5cb4e084
codermahbub20 <mkrefat5@gmail.com>
2024-11-22 22:46:02 +0600

    Successfully one data insert in database

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { BookRoutes } from './app/modules/book/book.route';
 const app: Application = express();
 
 // parsers use
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+//  application routes
+app.use('/api', BookRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -4,3 +4,5 @@ import { BookControllers } from './book.controler';
 const router = express.Router();
 
 router.post('/products', BookControllers.createProducts);
+
+export const BookRoutes = router;

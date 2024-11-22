87a134bdc0b442c5c7b62cae791cfdb60dac6185
codermahbub20 <mkrefat5@gmail.com>
2024-11-22 21:16:51 +0600

    Book Model are created

---
+++ src/app/modules/book/book.model.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+import { Book } from './book.interface';
+
+const bookSchema = new Schema<Book>({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ['Fiction', 'Science', 'SelfDevelopment', 'Poetry', 'Religious'],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+export const BookModel = model<Book>('Student', bookSchema);
